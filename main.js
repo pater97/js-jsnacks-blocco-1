a4f0383d36bcf0aebd44e04be207c25d14571374
pater97 <giuseppepater997@gmail.com>
2021-10-18 15:04:27 +0200

    impostazione pagina e inserimento consegne

---
+++ main.js
@@ -0,0 +1,60 @@
+//CONSEGNE:
+
+//JSNACK 1:
+/*
+ l'utente inserisce due numeri in successione, con due prompt. il software stampa il maggiore
+*/
+
+
+//JSNACK 2:
+/*
+l'utente inserisce due parole in successione , con due prompt , Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+
+//JSNACK 3: 
+/*
+Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+*/
+
+
+//JSNACK 4:
+/*
+In un array sono contenuti i nomi degli invitati alla festa del grande
+Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o
+no alla festa.
+*/
+
+
+//JSNACK 5:
+/*
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+se è dispari inseriscilo nell’array.
+*/
+
+
+//JSNACK 6:
+/*
+Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente.
+*/
+
+
+//JSNACK 7:
+/*
+Stampa le potenze di 2 fino a 1000.
+*/
+
+
+//JSNACK 8:
+/*
+Chiedi un numero di 4 cifre all’utente
+e calcola la somma di tutte le cifre che compongono il numero.
+*/
+
+
+//JSNAK 9:
+/*
+Calcola la somma e la media dei primi 10 numeri.
+*/
